843ef563f774561f181d65973737ec3a28c156da
sujingwen <137209038@qq.com>
2017-04-12 09:35:54 +0800

    代理

--- webpack-config/dev-server.js
+++ webpack-config/dev-server.js
@@ -7,12 +7,12 @@ const devServer = {
     colors: true
   },
   headers:{
-    "set-cookie":'JSESSIONID=9DC6D71DFD74247B9F0DB4531A8FB13D'
+    "set-cookie":'JSESSIONID=0FE58D5F5CCC378672584EA7DDDA5A99'
   },
   proxy: {
     '/bi/*': {
-       // target: 'http://172.16.72.14:8000/',
-      target: 'http://172.16.72.2:8080/',
+       target: 'http://172.16.72.14:8000/',
+      // target: 'http://172.16.72.2:8080/',
      // target: 'http://10.251.92.179:80/',
       changeOrigin: true
       /*rewrite: function(req) {
